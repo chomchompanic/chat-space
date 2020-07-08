cd39057dde54bd76e0da2c61c3b529992b8b83e3
chomchompanic <chomurashouta@gmail.com>
2020-07-08 21:05:37 +0900

    create pull request

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update]
-=======
-  get 'messages/index'
-
-  root "messages#index"
->>>>>>> Stashed changes
 end
